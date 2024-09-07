92511ea86bf5ffdc66ce00a6a5b7ee35483cb78f
Yuriekokubu <60240233118@udru.ac.th>
2024-09-07 12:58:07 +0700

    add more fnc to greeting

--- src/greeting.tsx
+++ src/greeting.tsx
@@ -8,15 +8,25 @@ function greeting(options: GreetingOptions): void {
     const { name, age, format } = options;
 
     if (format === "Table") {
-        console.log(`Hi Name: ${name}, Age: ${age}`);
-    }
-    else {
+        console.table({ Name: name, Age: age });
+    } else if (format === "Default") {
+        console.log(`Hi ${name}, Age: ${age}`);
+    } else {
         console.log("Unknown format");
     }
 }
+
 
 greeting({
     name: "Alice",
     age: 18,
     format: "Table"
 });
+
+greeting({
+    name: "Bob",
+    age: 25,
+    format: "Default"
+});
+
+greeting({ name: "Charlie", age: 18, format: "test_error" })
